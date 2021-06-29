3fda4ce38977634bd7709595801b148682de0215
kpzip <kpzipgames@gmail.com>
2021-06-29 14:34:51 -0700

    filled out mods.toml file

--- src/main/java/io/github/kpzip/slimerevolution/SlimeRevolution.java
+++ src/main/java/io/github/kpzip/slimerevolution/SlimeRevolution.java
@@ -9,15 +9,13 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("slimerevoulution")
 public class SlimeRevolution
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public SlimeRevolution() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
