25d1c6c730b85d273844ae68c419812bf41f5897
kpzip <kpzipgames@gmail.com>
2021-06-30 13:53:03 -0700

    editing blockstate code

--- src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockIndustrialBrewerController.java
+++ src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockIndustrialBrewerController.java
@@ -1,9 +1,23 @@
 package io.github.kpzip.slimerevolution.common.blocks;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
 public class BlockIndustrialBrewerController extends BlockMultiblockComponent {
 
 	public BlockIndustrialBrewerController(Properties property) {
 		super(property);
+	}
+
+	@Override
+	public boolean isMultiBlock(BlockState state, World world, BlockPos pos) {
+		return false;
+	}
+
+	@Override
+	public void updateMultiBlockProperty(BlockState state, World world, BlockPos pos) {
+		
 	}
 
 }
--- src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockMultiblockComponent.java
+++ src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockMultiblockComponent.java
@@ -2,12 +2,15 @@ package io.github.kpzip.slimerevolution.common.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.World;
 
-public class BlockMultiblockComponent extends BlockHorizontal {
+public abstract class BlockMultiblockComponent extends BlockHorizontal {
 	
 	public static final BooleanProperty IS_MULTIBLOCK = BooleanProperty.create("is_multiblock");
 
@@ -22,9 +25,25 @@ public class BlockMultiblockComponent extends BlockHorizontal {
 	}
 	
 	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return super.getStateForPlacement(context).setValue(IS_MULTIBLOCK, false);
+	}
+	
+	@Override
 	protected void createBlockStateDefinition(Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
 		builder.add(IS_MULTIBLOCK);
 	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void observedNeighborChange(BlockState observerState, World world, BlockPos observerPos, Block changedBlock, BlockPos changedBlockPos) {
+		super.observedNeighborChange(observerState, world, observerPos, changedBlock, changedBlockPos);
+		if (isMultiBlock(observerState, world, observerPos)) updateMultiBlockProperty(observerState, world, observerPos);
+	}
+	
+	public abstract boolean isMultiBlock(BlockState state, World world, BlockPos pos);
+	
+	public abstract void updateMultiBlockProperty(BlockState state, World world, BlockPos pos);
 
 }
