572273c2d74d791a32704b4e9bf78a236e495311
kpzip <kpzipgames@gmail.com>
2021-06-29 20:17:04 -0700

    progress on custom block shapes

--- src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockHorizontal.java
+++ src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockHorizontal.java
@@ -1,17 +1,80 @@
 package io.github.kpzip.slimerevolution.common.blocks;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
 public class BlockHorizontal extends Block {
 	
 	public static final DirectionProperty HORIZONAL_FACING = BlockStateProperties.HORIZONTAL_FACING;
+	
+	protected final Map<Direction, VoxelShape> shapes = new HashMap<Direction, VoxelShape>();
 
 	public BlockHorizontal(Properties property) {
 		super(property);
 		this.registerDefaultState(this.stateDefinition.any().setValue(HORIZONAL_FACING, Direction.NORTH));
+		runCalculation(shape);
 	}
-
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return state.rotate(mirrorIn.getRotation(state.getValue(HORIZONAL_FACING)));
+	}
+	
+	@Override
+	public BlockState rotate(BlockState state, IWorld world, BlockPos pos, Rotation direction) {
+		return state.setValue(HORIZONAL_FACING, direction.rotate(state.getValue(HORIZONAL_FACING)));
+	}
+	
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return this.defaultBlockState().setValue(HORIZONAL_FACING, context.getHorizontalDirection().getOpposite());
+	}
+	
+	@Override
+	protected void createBlockStateDefinition(Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(HORIZONAL_FACING);
+	}
+	
+	protected void calculateShapes(Direction to, VoxelShape shape) {
+	    VoxelShape[] buffer = new VoxelShape[] { shape, VoxelShapes.empty() };
+	 
+	    int times = (to.get2DDataValue() - Direction.NORTH.get2DDataValue() + 4) % 4;
+	    for (int i = 0; i < times; i++) {
+	        buffer[0].forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = VoxelShapes.or(buffer[1], VoxelShapes.create(new AxisAlignedBB(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX))));
+	        buffer[0] = buffer[1];
+	        buffer[1] = VoxelShapes.empty();
+	    }
+	 
+	    shapes.put(to, buffer[0]);
+	}
+	 
+	protected void runCalculation(VoxelShape shape) {
+	    for (Direction direction : Direction.values()) {
+	        calculateShapes(direction, shape);
+	    }
+	}
+	
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context) {
+		return shapes.get(state.getValue(HORIZONAL_FACING));
+	}
 }
