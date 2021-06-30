1ca69f65d47201c0f0a5566519bdefac0dcf4dec
kpzip <kpzipgames@gmail.com>
2021-06-29 18:28:16 -0700

    edited properties

--- src/main/java/io/github/kpzip/slimerevolution/core/init/BlockInit.java
+++ src/main/java/io/github/kpzip/slimerevolution/core/init/BlockInit.java
@@ -3,6 +3,7 @@ package io.github.kpzip.slimerevolution.core.init;
 import io.github.kpzip.slimerevolution.ModVars;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
@@ -15,8 +16,10 @@ public class BlockInit {
 	
 	public static final RegistryObject<Block> SLIME_BASED_RUBBER_BLOCK = BLOCKS.register("slime_based_rubber_block",
 			() -> new Block(AbstractBlock.Properties
-					.of(Material.SPONGE, MaterialColor.COLOR_BROWN)
+					.of(Material.CAKE, MaterialColor.COLOR_BROWN)
 					.harvestLevel(0)
 					.instabreak()
+					.sound(SoundType.SLIME_BLOCK)
+					.isRedstoneConductor((state, reader, pos) -> false)
 					));
 }
