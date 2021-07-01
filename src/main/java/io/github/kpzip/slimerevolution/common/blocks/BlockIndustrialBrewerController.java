3a47f7b72edad154ed8521446179cf4260f6f892
kpzip <kpzipgames@gmail.com>
2021-07-01 11:17:01 -0700

    progress on multiblock code

--- src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockIndustrialBrewerController.java
+++ src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockIndustrialBrewerController.java
@@ -1,5 +1,6 @@
 package io.github.kpzip.slimerevolution.common.blocks;
 
+import io.github.kpzip.slimerevolution.core.init.BlockInit;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.math.BlockPos;
@@ -13,18 +14,25 @@ public class BlockIndustrialBrewerController extends BlockMultiblockComponent {
 
 	@Override
 	public boolean isMultiBlock(BlockState state, World world, BlockPos pos) {
-		return false;
+		return world.getBlockState(pos.above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get())
+				&& world.getBlockState(pos.above().above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get()) 
+				&& world.getBlockState(pos.above()).getValue(BlockHorizontal.HORIZONAL_FACING).equals(state.getValue(BlockHorizontal.HORIZONAL_FACING)) 
+				&& world.getBlockState(pos.above().above()).getValue(BlockHorizontal.HORIZONAL_FACING).equals(state.getValue(BlockHorizontal.HORIZONAL_FACING));
 	}
 
 	@Override
 	public void addMultiBlockProperty(BlockState state, World world, BlockPos pos) {
-		
+		world.setBlockAndUpdate(pos, state.setValue(BlockMultiblockComponent.IS_MULTIBLOCK, true));
+		world.setBlockAndUpdate(pos.above(), world.getBlockState(pos.above()).setValue(BlockMultiblockComponent.IS_MULTIBLOCK, true));
+		world.setBlockAndUpdate(pos.above().above(), world.getBlockState(pos.above().above()).setValue(BlockMultiblockComponent.IS_MULTIBLOCK, true));
 		
 	}
 
 	@Override
 	public void removeMultiBlockProperty(BlockState state, World world, BlockPos pos) {
-		
+		if (state.getBlock().equals(BlockInit.INDUSTRIAL_BREWER_CONTROLLER.get())) world.setBlockAndUpdate(pos, state.setValue(BlockMultiblockComponent.IS_MULTIBLOCK, false));
+		if (world.getBlockState(pos.above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get())) world.setBlockAndUpdate(pos.above(), world.getBlockState(pos.above()).setValue(BlockMultiblockComponent.IS_MULTIBLOCK, false));
+		if (world.getBlockState(pos.above().above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get())) world.setBlockAndUpdate(pos.above().above(), world.getBlockState(pos.above().above()).setValue(BlockMultiblockComponent.IS_MULTIBLOCK, false));
 		
 	}
 
