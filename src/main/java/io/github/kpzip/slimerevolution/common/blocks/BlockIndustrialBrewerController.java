7b4a7772bcebbd867e42f87fe71dea01f48d31c7
kpzip <kpzipgames@gmail.com>
2021-07-01 18:19:27 -0700

    fixing bugs

--- src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockIndustrialBrewerController.java
+++ src/main/java/io/github/kpzip/slimerevolution/common/blocks/BlockIndustrialBrewerController.java
@@ -15,7 +15,7 @@ public class BlockIndustrialBrewerController extends BlockMultiblockComponent {
 	@Override
 	public boolean isMultiBlock(BlockState state, World world, BlockPos pos) {
 		return world.getBlockState(pos.above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get())
-				&& world.getBlockState(pos.above().above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get()) 
+				&& world.getBlockState(pos.above().above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_CHAMBER.get()) 
 				&& world.getBlockState(pos.above()).getValue(BlockHorizontal.HORIZONAL_FACING).equals(state.getValue(BlockHorizontal.HORIZONAL_FACING)) 
 				&& world.getBlockState(pos.above().above()).getValue(BlockHorizontal.HORIZONAL_FACING).equals(state.getValue(BlockHorizontal.HORIZONAL_FACING));
 	}
@@ -32,7 +32,7 @@ public class BlockIndustrialBrewerController extends BlockMultiblockComponent {
 	public void removeMultiBlockProperty(BlockState state, World world, BlockPos pos) {
 		if (state.getBlock().equals(BlockInit.INDUSTRIAL_BREWER_CONTROLLER.get())) world.setBlockAndUpdate(pos, state.setValue(BlockMultiblockComponent.IS_MULTIBLOCK, false));
 		if (world.getBlockState(pos.above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get())) world.setBlockAndUpdate(pos.above(), world.getBlockState(pos.above()).setValue(BlockMultiblockComponent.IS_MULTIBLOCK, false));
-		if (world.getBlockState(pos.above().above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_COLUMN.get())) world.setBlockAndUpdate(pos.above().above(), world.getBlockState(pos.above().above()).setValue(BlockMultiblockComponent.IS_MULTIBLOCK, false));
+		if (world.getBlockState(pos.above().above()).getBlock().equals(BlockInit.INDUSTRIAL_BREWER_CHAMBER.get())) world.setBlockAndUpdate(pos.above().above(), world.getBlockState(pos.above().above()).setValue(BlockMultiblockComponent.IS_MULTIBLOCK, false));
 		
 	}
 
